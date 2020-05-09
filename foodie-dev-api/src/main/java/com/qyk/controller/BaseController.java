44bc21b17c452172173fac8371a6fb583290abff
qiyankai <1694577129@qq.com>
2020-05-09 14:20:58 +0800

    个人中心        -代码优化

--- foodie-dev-api/src/main/java/com/qyk/controller/BaseController.java
+++ foodie-dev-api/src/main/java/com/qyk/controller/BaseController.java
@@ -16,6 +16,7 @@ public class BaseController {
     public String paymentUrl = "http://payment.t.mukewang.com/foodie-payment/payment/createMerchantOrder";
     // 待更新
 
+
     // 用户上传头像位置
     public static final String IMAGE_USER_FILE_LOCATION = File.separator + "Users" +
             File.separator + "qiyankai" +
