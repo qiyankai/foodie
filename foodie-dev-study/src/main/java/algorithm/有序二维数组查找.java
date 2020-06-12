334a795831331fd71147df59bcadbbb19fd724dc
qiyankai <1694577129@qq.com>
2020-06-12 17:02:43 +0800

    更新算法：二维矩形有序数组，查找数据

---
+++ foodie-dev-study/src/main/java/algorithm/有序二维数组查找.java
@@ -0,0 +1,13 @@
+package algorithm;
+
+public class 有序二维数组查找 {
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        for (int i = 0; i < matrix[0].length; i++) {
+
+            int left = 0, right = matrix[0].length,mid = 0;
+
+
+        }
+        return false;
+    }
+}
