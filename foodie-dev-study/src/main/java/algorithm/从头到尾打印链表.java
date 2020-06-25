ec8ad62ee2113aff70a5aae58b1de88f6c836742
qiyankai <1694577129@qq.com>
2020-06-25 10:49:36 +0800

    更新算法：打印链表

---
+++ foodie-dev-study/src/main/java/algorithm/从头到尾打印链表.java
@@ -0,0 +1,31 @@
+package algorithm;
+
+import java.util.Stack;
+
+public class 从头到尾打印链表 {
+    public int[] reversePrint(ListNode head) {
+        int len = 0;
+        ListNode currNode = head;
+        while (currNode != null) {
+            len++;
+            currNode = currNode.next;
+        }
+        int[] resultArr = new int[len];
+        currNode = head;
+        while (currNode != null) {
+            resultArr[len--]=currNode.val;
+            currNode = currNode.next;
+        }
+        return resultArr;
+    }
+}
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
